a191f57696e83f078a63979fab54f378a613c0dd
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-01-02 21:27:37 +0530

    added button component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import Button from '@/components/ui/Button'
+interface pageProps {
+
+}
+
+const page: FC<pageProps> = ({ }) => {
+    return <Button>Hello</Button>
+}
+
+export default page
