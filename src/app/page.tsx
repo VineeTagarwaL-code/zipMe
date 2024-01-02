104ab6df3b7e51f7a5c4e475d2b958e08f749259
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-01-02 22:13:55 +0530

    build : redis integration and next auth integration , some bugs exists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { db } from '@/lib/db'
 import Image from 'next/image'
 
 export default function Home() {
+  db.set('hello', 'hello')
   return (
     <div className='text-red-500'>Hey</div>
   )
---
+++ src/lib/auth.ts
@@ -0,0 +1,16 @@
+import { NextAuthOptions } from "next-auth";
+import { UpstashRedisAdapter } from "@auth/upstash-redis-adapter";
+import { db } from "./db";
+import GoogleProvider from "next-auth/providers/google";
+export const authOptions: NextAuthOptions = {
+    adapter: UpstashRedisAdapter(db),
+    session: {
+        strategy: 'jwt'
+    },
+    pages: {
+        signIn: '/login'
+    },
+    providers: [
+        GoogleProvider({})
+    ]
+}
